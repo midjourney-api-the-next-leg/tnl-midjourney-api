77c999f4273829cee40e387820379377f4439dd7
njho <support@thenextleg.io>
2023-04-30 19:41:58 -0600

    build: initial Commit

---
+++ README.md
@@ -0,0 +1,127 @@
+# The Next Leg
+
+The Next Leg is a module that provides functionality for creating AI-generated images with Midjourney. It provides a simple interface for interacting with Midjourney's API and performing various actions such as creating images from prompts or URLs, describing images, using buttons or slash commands, and getting account settings.
+
+## Installation
+
+To use this module, you need to have Node.js installed on your machine. To install this module, run the following command:
+
+```sh
+npm install tnl-midjourney-api
+```
+
+## Usage
+
+Here is an example of how to use the module to create an image from a prompt:
+
+```javascript
+const { TNL } = require('tnl-midjourney-api');
+
+const TNL_API_KEY = 'your_api_key_here';
+const tnl = new TNL(TNL_API_KEY);
+
+const prompt = 'a cat playing the piano';
+const response = await tnl.imagine(prompt);
+
+console.log(response);
+```
+
+## API
+
+### `new TNL(apiKey: string)`
+
+Creates a new instance of `TNL` with the provided `apiKey`.
+
+### Imagine
+
+`tnl.imagine(prompt: string, ref?: string, webhookOverride?: string): Promise<TNLTypes.Response.Message>`
+
+Creates a new image from a prompt.
+
+- `prompt` - The prompt you want to use to generate the image.
+- `ref` (optional) - A reference string that will be returned in the webhook response.
+- `webhookOverride` (optional) - A webhook URL that will be used instead of the one set in the dashboard.
+
+### Get Progress and Message Result
+
+`tnl.getProgressAndMessage(messageId: string, expireMins?: number): Promise<TNLTypes.Webhooks.WebhookResponse>`
+
+Gets the progress and response of a message.
+
+- `messageId` - The message ID of the message you want to get the progress and response for.
+- `expireMins` (optional) - A timeout for the request in minutes. If the request takes longer than this, it will return as 'incomplete'
+
+### Img 2 Img
+
+`tnl.img2img(prompt: string, imgUrl: string, ref?: string, webhookOverride?: string): Promise<TNLTypes.Response.Message>`
+
+Creates an image from a prompt and an image.
+
+- `prompt` - The prompt you want to use to generate the image.
+- `imgUrl` - The URL of the image you want to use as the base image.
+- `ref` (optional) - A reference string that will be returned in the webhook response.
+- `webhookOverride` (optional) - A webhook URL that will be used instead of the one set in the dashboard.
+
+### Describe
+
+`tnl.describe(imgUrl: string, ref?: string, webhookOverride?: string): Promise<TNLTypes.Response.Message>`
+
+Describes an image.
+
+- `imgUrl` - The URL of the image you want to describe.
+- `ref` (optional) - A reference string that will be returned in the webhook response.
+- `webhookOverride` (optional) - A webhook URL that will be used instead of the one set in the dashboard.
+
+### Button
+
+`tnl.button(button: TNLTypes.ButtonTypes, buttonMessageId: string, ref?: string, webhookOverride?: string): Promise<TNLTypes.Response.Message>`
+
+Uses a button on an image.
+
+- `button` - A button type.
+- `buttonMessageId` - The buttonMessageId of the message that contains the button.
+- `ref` (optional) - A reference string that will be returned in the webhook response.
+- `webhookOverride` (optional) - A webhook URL that will be used instead of the one set in the dashboard.
+
+### Get Seed
+
+`tnl.getSeed(messageId: string): Promise<TNLTypes.Response.Seed>`
+
+Gets a seed of a message.
+
+- `messageId` - The message ID of the message you want to get the seed for.
+
+### Slash Command
+
+`tnl.slashCommand(slashCommand: TNLTypes.SlashCommands, ref?: string, webhookOverride?: string): Promise<TNLTypes.Response.Message>`
+
+Uses a slash command such as relax, fast, private, or stealth.
+
+- `slashCommand` - A slash command type.
+- `ref` (optional) - A reference string that will be returned in the webhook response.
+- `webhookOverride` (optional) - A webhook URL that will be used instead of the one set in the dashboard.
+
+### Get Settings
+
+`tnl.getSettings(): Promise<TNLTypes.Response.Message>`
+
+Gets the settings available for your account.
+
+### Set Settings
+
+`tnl.setSettings(settings: TNLTypes.Settings, ref?: string, webhookOverride?: string): Promise<TNLTypes.Response.Message>`
+
+Sets the settings for your account.
+
+- `settings` - The settings you want to set.
+- `ref` (optional) - A reference string that will be returned in the webhook response.
+- `webhookOverride` (optional) - A webhook URL that will be used instead of the one set in the dashboard.
+
+### Get Info
+
+`tnl.getInfo(ref?: string, webhookOverride?: string): Promise<TNLTypes.Response.Message>`
+
+Gets information about your account including Fast Time Remaining, Job Mode, Queued Jobs and more.
+
+- `ref` (optional) - A reference string that will be returned in the webhook response.
+- `webhookOverride` (optional) - A webhook URL that will be used instead of the one set in the dashboard.
---
+++ src/index.ts
@@ -0,0 +1,343 @@
+'use strict';
+
+import axios from 'axios';
+
+const BASE_URL = 'https://api.thenextleg.io/v2';
+
+export declare namespace TNLTypes {
+  type ButtonTypes =
+    | 'U1'
+    | 'U2'
+    | 'U3'
+    | 'U4'
+    | 'V1'
+    | 'V2'
+    | 'V3'
+    | 'V3'
+    | '🔄'
+    | '🪄 Make Variations'
+    | '❤️ Favorite';
+  type SlashCommands = 'relax' | 'fast' | 'private' | 'stealth';
+
+  type Settings =
+    | 'MJ version 1'
+    | 'MJ version 2'
+    | 'MJ version 3'
+    | 'MJ version 4'
+    | 'MJ version 5'
+    | 'Niji version 4'
+    | 'Niji version 5'
+    | 'MJ Test'
+    | 'MJ Test Photo'
+    | 'Half quality'
+    | 'Base quality'
+    | 'Base quality'
+    | 'High quality (2x cost)'
+    | 'Style low'
+    | 'Style med'
+    | 'Style high'
+    | 'Style very high'
+    | 'Reset Settings'
+    | 'Public mode'
+    | 'Stealth mode'
+    | 'Remix mode'
+    | 'Fast mode'
+    | 'Relax mode';
+
+  namespace Request {
+    interface BaseRequest {
+      ref: string;
+      webhookOverride: string;
+    }
+
+    interface Imagine extends BaseRequest {
+      msg: string;
+    }
+
+    interface SlashCommand extends BaseRequest {
+      cmd: SlashCommands;
+    }
+
+    interface SetSettings extends BaseRequest {
+      settingsToggle: Settings;
+    }
+    type Info = BaseRequest;
+
+    interface Buttons extends BaseRequest {
+      cmd: ButtonTypes;
+      buttonMessageId: string;
+    }
+
+    interface Describe extends BaseRequest {
+      url: string;
+    }
+  }
+  namespace Response {
+    type Message = {
+      createdAt: string;
+      messageId: string;
+      success: boolean;
+    };
+    type Seed = {
+      seed: string;
+    };
+  }
+
+  namespace WebhookResponses {
+    interface BaseResponse {
+      ref: string;
+      createdAt: string;
+      responseAt: string;
+      originatingMessageId: string;
+      buttonMessageId: string;
+      imageUrl: string;
+      buttons: string[];
+    }
+    interface Imagine extends BaseResponse {
+      content: string;
+    }
+    type Button = Imagine;
+    interface Describe extends BaseResponse {
+      content: string[] | string;
+      type: 'describe';
+    }
+    interface SlashCommand extends BaseResponse {
+      content: string;
+    }
+    interface Settings extends BaseResponse {
+      content: string;
+    }
+    interface Info extends BaseResponse {
+      content: {
+        'Fast Time Remaining': string;
+        'Job Mode': string;
+        'Lifetime Usage': string;
+        'Queued Jobs (fast)': string;
+        'Queued Jobs (relax)': string;
+        'Relaxed Usage': string;
+        Subscription: string;
+        'Visibility Mode': string;
+      };
+      type: 'info';
+    }
+  }
+}
+
+export class TNL {
+  /* Private Instance Fields */
+  private token: string;
+
+  /* Constructor */
+  constructor(token: string) {
+    this.token = token;
+  }
+
+  private createHeaders() {
+    return {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    };
+  }
+
+  /**
+   * Create a new image from a prompt
+   * @param prompt - The prompt you want to use to generate the image
+   * @param ref - A reference string that will be returned in the webhook response
+   * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
+   */
+  public async imagine(
+    prompt: string,
+    ref = '',
+    webhookOverride = '',
+  ): Promise<TNLTypes.Response.Message> {
+    const request: TNLTypes.Request.Imagine = {
+      msg: prompt,
+      ref,
+      webhookOverride,
+    };
+
+    const res = await axios.post(`${BASE_URL}/imagine`, request, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+
+  /**
+   * Create an image from a prompt and an image
+   * @param prompt - The prompt you want to use to generate the image
+   * @param imgUrl - The URL of the image you want to use as the base image
+   * @param ref - A reference string that will be returned in the webhook response
+   * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
+   */
+  public async img2img(
+    prompt: string,
+    imgUrl: string,
+    ref = '',
+    webhookOverride = '',
+  ): Promise<TNLTypes.Response.Message> {
+    const request: TNLTypes.Request.Imagine = {
+      msg: `${imgUrl} ${prompt}`,
+      ref,
+      webhookOverride,
+    };
+
+    const res = await axios.post(`${BASE_URL}/imagine`, request, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+
+  /**
+   * Describe an image
+   * @param imgUrl - The URL of the image you want to describe
+   * @param ref - A reference string that will be returned in the webhook response
+   * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
+   */
+  public async describe(
+    imgUrl: string,
+    ref = '',
+    webhookOverride = '',
+  ): Promise<TNLTypes.Response.Message> {
+    const request: TNLTypes.Request.Describe = {
+      url: imgUrl,
+      ref,
+      webhookOverride,
+    };
+    const res = await axios.post(`${BASE_URL}/imagine`, request, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+
+  /**
+   * Use a button on an image. This can include upscale, variation, re-roll and more.
+   * @param button - A button type
+   * @param buttonMessageId - The buttonMessageId of the message that contains the button
+   * @param ref - A reference string that will be returned in the webhook response
+   * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
+   */
+  public async button(
+    button: TNLTypes.ButtonTypes,
+    buttonMessageId: string,
+    ref = '',
+    webhookOverride = '',
+  ): Promise<TNLTypes.Response.Message> {
+    const request: TNLTypes.Request.Buttons = {
+      cmd: button,
+      buttonMessageId,
+      ref,
+      webhookOverride,
+    };
+    const res = await axios.post(`${BASE_URL}/button`, request, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+
+  /**
+   * Get a seed of a message
+   * @param messageId - The message ID of the message you want to get the seed for
+   */
+  public async getSeed(messageId: string) {
+    const request = {
+      messageId,
+    };
+    const res = await axios.post(`${BASE_URL}/seed`, request, {
+      headers: this.createHeaders(),
+    });
+
+    return res.data as TNLTypes.Response.Seed;
+  }
+
+  /**
+   * Use a slash command such as relax, fast, private, or stealth
+   * @param slashCommand - A slash command type
+   * @param ref - A reference string that will be returned in the webhook response
+   * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
+   */
+  public async slashCommand(
+    slashCommand: TNLTypes.SlashCommands,
+    ref = '',
+    webhookOverride = '',
+  ): Promise<TNLTypes.Response.Message> {
+    const request: TNLTypes.Request.SlashCommand = {
+      cmd: slashCommand,
+      ref,
+      webhookOverride,
+    };
+
+    const res = await axios.post(`${BASE_URL}/slash-commands`, request, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+
+  /**
+   * Get the settings available on your account
+   */
+  public async getSettings() {
+    const res = await axios.get(`${BASE_URL}/settings`, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+
+  /**
+   * Set a setting on your account. You should use `getSettings()` in order to retrieve the settings available in your account.
+   * @param setting - The setting you want to change
+   * @param ref - A reference string that will be returned in the webhook response
+   * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
+   */
+  public async setSettings(
+    setting: TNLTypes.Settings,
+    ref = '',
+    webhookOverride = '',
+  ) {
+    const request: TNLTypes.Request.SetSettings = {
+      settingsToggle: setting,
+      ref,
+      webhookOverride,
+    };
+
+    const res = await axios.post(`${BASE_URL}/settings`, request, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+
+  /**
+   * Get Information about your account including Fast Time Remaining, Job Mode, Queued Jobs and more.
+   * @param ref - A reference string that will be returned in the webhook response
+   * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
+   * @returns
+   */
+  public async getInfo(ref = '', webhookOverride = '') {
+    const request: TNLTypes.Request.Info = {
+      ref,
+      webhookOverride,
+    };
+
+    const res = await axios.post(`${BASE_URL}/info`, request, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+
+  /**
+   * Get the progress and status of any message that you have sent
+   * @param messageId - The message ID of the message you want to get the progress of
+   * @param expireMins - A timeout for the request in minutes. If the request takes longer than this, it will return as 'incomplete'
+   * @returns
+   */
+  public async getProgressAndMessage(messageId: string, expireMins?: number) {
+    let url = `${BASE_URL}/message/${messageId}`;
+
+    if (expireMins) {
+      url += `?expireMins=${expireMins}`;
+    }
+    const res = await axios.get(url, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.Message;
+  }
+}
