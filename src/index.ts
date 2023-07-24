2515728833cffc230180e9ff8f7f96fe9a3fb357
Agusti Dosaiguas <agusti.dosaiguas@siriondev.com>
2023-07-25 00:47:52 +0200

    Add loadBalanceId to TNLBalanced.button and fixes

--- src/index.ts
+++ src/index.ts
@@ -70,6 +70,10 @@ export declare namespace TNLTypes {
       buttonMessageId: string;
     }
 
+    interface ButtonsLB extends Buttons {
+      loadBalanceId: string;
+    }
+
     interface Describe extends BaseRequest {
       url: string;
     }
@@ -81,13 +85,10 @@ export declare namespace TNLTypes {
       messageId: string;
       success: boolean;
     };
-    type MessageLB = {
-      createdAt: string;
-      messageId: string;
-      success: boolean;
-      loadBalanceId: string,
-      accountId: string
-    };
+    interface MessageLB extends Message {
+      loadBalanceId: string;
+      accountId: string;
+    }
     interface MessageAndProgress extends Request.BaseRequest {
       progress: number | 'incomplete';
       response:
@@ -258,7 +259,7 @@ export class TNL {
    * Get a seed of a message
    * @param messageId - The message ID of the message you want to get the seed for
    */
-  public async getSeed(messageId: string) {
+  public async getSeed(messageId: string): Promise<TNLTypes.Response.Seed> {
     const request = {
       messageId,
     };
@@ -295,7 +296,7 @@ export class TNL {
   /**
    * Get the settings available on your account
    */
-  public async getSettings() {
+  public async getSettings(): Promise<TNLTypes.Response.Message> {
     const res = await axios.get(`${BASE_URL}/settings`, {
       headers: this.createHeaders(),
     });
@@ -312,7 +313,7 @@ export class TNL {
     setting: TNLTypes.Settings,
     ref = '',
     webhookOverride = '',
-  ) {
+  ): Promise<TNLTypes.Response.Message> {
     const request: TNLTypes.Request.SetSettings = {
       settingsToggle: setting,
       ref,
@@ -328,7 +329,7 @@ export class TNL {
   /**
    * Get Information about your account including Fast Time Remaining, Job Mode, Queued Jobs and more.
    */
-  public async getInfo() {
+  public async getInfo(): Promise<TNLTypes.Response.Message> {
     const res = await axios.get(`${BASE_URL}/info`, {
       headers: this.createHeaders(),
     });
@@ -340,7 +341,7 @@ export class TNL {
    * @param messageId - The message ID of the message you want to get the progress of
    * @param expireMins - A timeout for the request in minutes. If the request takes longer than this, it will return as 'incomplete'
    */
-  public async getMessageAndProgress(messageId: string, expireMins?: number) {
+  public async getMessageAndProgress(messageId: string, expireMins?: number): Promise<TNLTypes.Response.MessageAndProgress> {
     let url = `${BASE_URL}/message/${messageId}`;
 
     if (expireMins) {
@@ -392,26 +393,49 @@ export class TNLBalanced {
     return res.data as TNLTypes.Response.MessageLB;
   }
 
+  /**
+   * Describe an image
+   * @param imgUrl - The URL of the image you want to describe
+   * @param ref - A reference string that will be returned in the webhook response
+   * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
+   */
+  public async describe(
+    imgUrl: string,
+    ref = '',
+    webhookOverride = '',
+  ): Promise<TNLTypes.Response.MessageLB> {
+    const request: TNLTypes.Request.Describe = {
+      url: imgUrl,
+      ref,
+      webhookOverride,
+    };
+    const res = await axios.post(`${BASE_URL_LOAD_BALANCER}/describe`, request, {
+      headers: this.createHeaders(),
+    });
+    return res.data as TNLTypes.Response.MessageLB;
+  }
 
   /**
    * Use a button on an image. This can include upscale, variation, re-roll and more.
    * @param button - A button type
    * @param buttonMessageId - The buttonMessageId of the message that contains the button
+   * @param loadBalanceId - The loadBalanceId received in the response to the imagine call
    * @param ref - A reference string that will be returned in the webhook response
    * @param webhookOverride - A webhook URL that will be used instead of the one set in the dashboard
    */
   public async button(
     button: TNLTypes.ButtonTypes,
     buttonMessageId: string,
+    loadBalanceId: string,
     ref = '',
-    webhookOverride = '',
-
+    webhookOverride = '',    
   ): Promise<TNLTypes.Response.MessageLB> {
-    const request: TNLTypes.Request.Buttons = {
+    const request: TNLTypes.Request.ButtonsLB = {
       button,
       buttonMessageId,
       ref,
       webhookOverride,
+      loadBalanceId,
     };
 
     const res = await axios.post(`${BASE_URL_LOAD_BALANCER}/button`, request, {
@@ -423,13 +447,14 @@ export class TNLBalanced {
   /**
    * Get the progress and status of any message that you have sent
    * @param messageId - The message ID of the message you want to get the progress of
+   * @param loadBalanceId - The loadBalanceId received in the response to the imagine call
    * @param expireMins - A timeout for the request in minutes. If the request takes longer than this, it will return as 'incomplete'
    */
   public async getMessageAndProgress(
     messageId: string,
     loadBalanceId: string,
     expireMins?: number
-  ) {
+  ): Promise<TNLTypes.Response.MessageAndProgress> {
     let url = `${BASE_URL_LOAD_BALANCER}/message/${messageId}?loadBalanceId=${loadBalanceId}`;
 
     if (expireMins) {
